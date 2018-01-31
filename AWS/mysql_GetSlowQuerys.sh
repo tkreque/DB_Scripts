5f4d76e24f0e32dcd8cd5a6562d4cb91d96c4c2c
Thiago Reque <thiago.reque@4all.com>
2018-01-31 15:39:20 -0200

    Minor adjustment to AWS script

--- AWS/mysql_GetSlowQuerys.sh
+++ AWS/mysql_GetSlowQuerys.sh
@@ -2,10 +2,10 @@
 
 #################################################
 #												#
-#				AWS RDS search by Tag			#
+#				AWS Get Slow Query Log			#
 #	Created by - Thiago Reque					#
-#			Date: 			15/01/2018			#
-#			Last changed:	15/01/2018			#
+#			Date: 			30/01/2018			#
+#			Last changed:	30/01/2018			#
 #												#
 #	Change log:									#
 #		- Created the script					#
@@ -13,26 +13,48 @@
 #################################################
 
 
-# Ask the format or gets the default
+# Ask the path of the temp and final file
 echo "Path to file"
 read PATHFILE
 
-# Ask the format or gets the default
+# Ask the instance to connect
 echo "MySQL Instance"
 read MYSQL
 
-# Ask the format or gets the default
+# Ask the user
 echo "User"
 read MYSQLU
 
-# Ask the format or gets the default
+# Ask the password
 echo "Password"
 read MYSQLP
 
+# Adjust the path
+if [[ $PATHFILE != */ ]]; then 
+	PATHFILE=$PATHFILE'/'
+fi
+
+# Set filename
 FILENAME="SlowQuery-`date +%Y-%m-%d_%H-%M`-$MYSQL.txt"
 
-mysql -u$MYSQLU -p$MYSQLP -h $MYSQL -D mysql -s -r -e "SELECT CONCAT( '# Time: ', DATE_FORMAT(start_time, '%y%m%d %H%i%s'), '\n', '# User@Host: ', user_host, '\n', '# Query_time: ', TIME_TO_SEC(query_time),  '  Lock_time: ', TIME_TO_SEC(lock_time), '  Rows_sent: ', rows_sent, '  Rows_examined: ', rows_examined, '\n', sql_text, ';' ) FROM mysql.slow_log WHERE start_time > now() - interval 2 day AND user_host NOT IN ('rdsadmin[rdsadmin] @ localhost [127.0.0.1]') AND db NOT IN ('mysql','information_schema','tmp','performance_schema','sys','innodb') AND sql_text NOT LIKE '%ApplicationName=DBeaver%' AND sql_text NOT LIKE '%mysql.slow_log%'" > $PATHFILE/mysql.slow_log.log
-pt-query-digest --limit 100% $PATHFILE/mysql.slow_log.log > $PATHFILE/$FILENAME
-rm $PATHFILE/mysql.slow_log.log
+# Generate the temp file
+if mysql -u$MYSQLU -p$MYSQLP -h $MYSQL -D mysql -s -r -e "SELECT CONCAT( '# Time: ', DATE_FORMAT(start_time, '%y%m%d %H%i%s'), '\n', '# User@Host: ', user_host, '\n', '# Query_time: ', TIME_TO_SEC(query_time),  '  Lock_time: ', TIME_TO_SEC(lock_time), '  Rows_sent: ', rows_sent, '  Rows_examined: ', rows_examined, '\n', sql_text, ';' ) FROM mysql.slow_log WHERE start_time > now() - interval 2 day AND user_host NOT IN ('rdsadmin[rdsadmin] @ localhost [127.0.0.1]') AND db NOT IN ('mysql','information_schema','tmp','performance_schema','sys','innodb') AND sql_text NOT LIKE '%ApplicationName=DBeaver%' AND sql_text NOT LIKE '%mysql.slow_log%'" > $PATHFILE/mysql.slow_log.log; then
 
-ls -ltrh $PATHFILE/$FILENAME
+	# Generate the final file
+	# Require percona-toolkit (sudo apt install percona-toolkit)
+	if pt-query-digest --limit 100% $PATHFILE/mysql.slow_log.log > $PATHFILE/$FILENAME; then
+
+		# Remove the temp file
+		if rm $PATHFILE/mysql.slow_log.log; then
+
+			# Show the file created
+			ls -ltrh $PATHFILE/$FILENAME
+		else
+			echo "Error - Not able to remove temporary file! Check and try again!"
+		fi
+	else
+		echo "Error - Not able to generate the file! Check and try again! (Maybe you need to instal percona-toolkit ;)"
+	fi
+else
+	echo "Error - Not able to connect to MySQL! Check and try again!"
+fi
